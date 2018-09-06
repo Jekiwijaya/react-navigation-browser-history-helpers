3c60f9cdef36e80908e465ef9afd2d9880b01269
Adrian Hartanto <adrian.hartanto@traveloka.com>
2018-09-06 15:09:08 +0700

    Add navigatorRef handler on withBrowserHistory

--- es6/withBrowserHistory.js
+++ es6/withBrowserHistory.js
@@ -2,8 +2,20 @@ import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 
 export default function stubFunc(W){
-  const Comp = ({basePath, uriPrefix, ...rest}) => (
-    <W {...rest} />
-  );
+  const Comp = class extends React.Component {
+    render() {
+      const { navigatorRef, basePath, uriPrefix, ...rest } = this.props;
+
+      return (
+        <W
+          ref={ref => {
+            navigatorRef && navigatorRef(ref);
+          }}
+          {...rest}
+        />
+      )
+    }
+  }
+
   return hoistNonReactStatics(Comp, W);
 };
